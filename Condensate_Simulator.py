d0cb2bb91c6353a5b13b5f1efdef475431fce173
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-01-26 00:45:48 -0500

    Update Condensate_Simulator.py

--- Condensate_Simulator.py
+++ Condensate_Simulator.py
@@ -1,7 +1,9 @@
+from os.path import join
 import numpy as np
 from numpy.random import rand
 import pandas as pd
 from tifffile import imwrite
+from tkinter import filedialog as fd
 
 ##################################
 # Parameters
@@ -12,7 +14,7 @@ condensate_r_range = (100, 2000)  # unit: nm
 pad_size = 200  # push condensates back from FOV edges. unit: nm
 N_condensate = 1  # number of condensates per field of view
 N_fov = 100  # number of total im
-
+folder_save = fd.askdirectory()
 
 ##################################
 # Main
@@ -30,3 +32,22 @@ center_y = []
 for current_min, current_max in zip(coor_min, coor_max):
     center_x.append(rand() * (current_max - current_min) + current_min)
     center_y.append(rand() * (current_max - current_min) + current_min)
+# generate FOV index
+index = [np.repeat(x, N_condensate) for x in range(N_fov)]
+index = np.hstack(index)
+# Wrap up and save ground truth
+df_groundtruth = pd.DataFrame(
+    {
+        "FOVindex": index,
+        "x": center_x,
+        "y": center_y,
+        "r": condensate_r,
+        "r_min_nm": np.repeat(condensate_r_range[0], N_fov * N_condensate),
+        "r_max_nm": np.repeat(condensate_r_range[1], N_fov * N_condensate),
+        "FOVsize_nm": np.repeat(fovsize, N_fov * N_condensate),
+        "padding_nm": np.repeat(pad_size, N_fov * N_condensate),
+    },
+    dtype=object,
+)
+path_save = join(folder_save, "groundtruth.csv")
+df_groundtruth.to_csv(path_save, index=False)
