84271242e8346bf3fb26563384cd052496e625ab
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-02-03 14:32:42 -0500

    Update plot-validation.py

--- plot-validation.py
+++ plot-validation.py
@@ -28,25 +28,25 @@ def rmsd_d2edge(df):
         )
         current_d2edge = np.array([x for x in lst_current_d2edge if x], dtype=float)
         array_d2edge = np.concatenate((array_d2edge, current_d2edge))
-    rmsd_d2edge = np.sqrt(np.mean(current_d2edge**2))
+    rmsd_d2edge = np.sqrt(np.mean(array_d2edge**2))
 
     return rmsd_d2edge
 
 
 def compute_chunck(df, tag):
-    global df_truth
     rmsd_area_nm2 = []
     rmsd_aspect_ratio = []
     rmsd_distance2center = []
     rmsd_distance2edge = []
     for i in range(30):
         df_current = df.iloc[i * 100 : (i + 1) * 100 - 1]
-        df_ref = df_truth.iloc[i * 100 : (i + 1) * 100 - 1]
         rmsd_distance2edge.append(rmsd_d2edge(df_current))
-        rmsd_area_nm2.append(np.sqrt(np.mean(df_ref["rmsd_area_nm2"] ** 2)))
-        rmsd_aspect_ratio.append(np.sqrt(np.mean(df_ref["rmsd_aspect_ratio"] ** 2)))
+        rmsd_area_nm2.append(np.sqrt(np.nanmean(df_current["rmsd_area_nm2"] ** 2)))
+        rmsd_aspect_ratio.append(
+            np.sqrt(np.nanmean(df_current["rmsd_aspect_ratio"] ** 2))
+        )
         rmsd_distance2center.append(
-            np.sqrt(np.mean(df_ref["rmsd_distance2center"] ** 2))
+            np.sqrt(np.nanmean(df_current["rmsd_distance2center"] ** 2))
         )
     df_new = pd.DataFrame(
         {
@@ -76,7 +76,7 @@ df1 = compute_chunck(df_threshold, "Thresholding")
 
 
 # for method 2
-df_canny = pd.read_csv(path_threshold)
+df_canny = pd.read_csv(path_canny)
 df_canny["index"] = df_canny["index"].to_numpy(dtype=int)
 df_canny["rmsd_area_nm2"] = df_truth["area_nm2"] - df_canny["area_nm2"]
 df_canny["rmsd_aspect_ratio"] = 1 - df_canny["aspect_ratio"]
@@ -84,7 +84,7 @@ df_canny["rmsd_distance2center"] = df_canny["distance2center"]
 df2 = compute_chunck(df_canny, "CannyEdge")
 
 # for method 3
-df_blob = pd.read_csv(path_threshold)
+df_blob = pd.read_csv(path_blob)
 df_blob["index"] = df_blob["index"].to_numpy(dtype=int)
 df_blob["rmsd_area_nm2"] = df_truth["area_nm2"] - df_blob["area_nm2"]
 df_blob["rmsd_aspect_ratio"] = 1 - df_blob["aspect_ratio"]
@@ -92,7 +92,8 @@ df_blob["rmsd_distance2center"] = df_blob["distance2center"]
 df3 = compute_chunck(df_blob, "BlobDetector")
 
 data = pd.concat([df1, df2, df3])
-data.dropna(inplace=True)
+# data.dropna(inplace=True)
+data.to_csv("data.csv", index=False)
 
 # area
 x = "class"
@@ -123,4 +124,98 @@ plt.xticks(rotation=30)
 plt.ylabel("RMSD Area (nm${^2}$)")
 plt.tight_layout()
 plt.savefig("RMSD-Area.png", format="png", bbox_inches="tight")
+plt.close()
+
+# rmsd_aspect_ratio
+x = "class"
+y = "rmsd_aspect_ratio"
+order = ["Thresholding", "CannyEdge", "BlobDetector"]
+box_pairs = [
+    ("Thresholding", "CannyEdge"),
+    ("Thresholding", "BlobDetector"),
+    ("CannyEdge", "BlobDetector"),
+]
+plt.figure(figsize=(5, 6), dpi=200)
+ax = sns.boxplot(data=data, x=x, y=y, order=order)
+ax = sns.swarmplot(data=data, x=x, y=y, order=order, color=".25")
+test_results = add_stat_annotation(
+    ax,
+    data=data,
+    x=x,
+    y=y,
+    order=order,
+    box_pairs=box_pairs,
+    test="t-test_ind",
+    comparisons_correction=None,
+    text_format="star",
+    loc="inside",
+    verbose=2,
+)
+plt.xticks(rotation=30)
+plt.ylabel("RMSD Aspect Ratio")
+plt.tight_layout()
+plt.savefig("RMSD-AspectRatio.png", format="png", bbox_inches="tight")
+plt.close()
+
+# rmsd_distance2center
+x = "class"
+y = "rmsd_distance2center"
+order = ["Thresholding", "CannyEdge", "BlobDetector"]
+box_pairs = [
+    ("Thresholding", "CannyEdge"),
+    ("Thresholding", "BlobDetector"),
+    ("CannyEdge", "BlobDetector"),
+]
+plt.figure(figsize=(5, 6), dpi=200)
+ax = sns.boxplot(data=data, x=x, y=y, order=order)
+ax = sns.swarmplot(data=data, x=x, y=y, order=order, color=".25")
+test_results = add_stat_annotation(
+    ax,
+    data=data,
+    x=x,
+    y=y,
+    order=order,
+    box_pairs=box_pairs,
+    test="t-test_ind",
+    comparisons_correction=None,
+    text_format="star",
+    loc="inside",
+    verbose=2,
+)
+plt.xticks(rotation=30)
+plt.ylabel("RMSD, center (nm)")
+plt.tight_layout()
+plt.savefig("RMSD-center.png", format="png", bbox_inches="tight")
+plt.close()
+
+# rmsd_distance2edge
+
+x = "class"
+y = "rmsd_distance2edge"
+order = ["Thresholding", "CannyEdge", "BlobDetector"]
+box_pairs = [
+    ("Thresholding", "CannyEdge"),
+    ("Thresholding", "BlobDetector"),
+    ("CannyEdge", "BlobDetector"),
+]
+plt.figure(figsize=(5, 6), dpi=200)
+ax = sns.boxplot(data=data, x=x, y=y, order=order)
+ax = sns.swarmplot(data=data, x=x, y=y, order=order, color=".25")
+test_results = add_stat_annotation(
+    ax,
+    data=data,
+    x=x,
+    y=y,
+    order=order,
+    box_pairs=box_pairs,
+    test="t-test_ind",
+    comparisons_correction=None,
+    text_format="star",
+    loc="inside",
+    verbose=2,
+)
+plt.xticks(rotation=30)
+plt.ylabel("RMSD, edge (nm)")
+plt.tight_layout()
+plt.savefig("RMSD-edge.png", format="png", bbox_inches="tight")
 plt.close()
