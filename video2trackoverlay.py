6e30960d1d24bfda864debcff89eda0fad0c1fe1
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-02-05 23:53:13 -0500

    Create video2trackoverlay.py

---
+++ video2trackoverlay.py
@@ -0,0 +1,4 @@
+from tifffile import imread, imwrite
+
+path = "/Users/GGM/Documents/Graduate_Work/Nils_Walter_Lab/Writing/MyPublications/ResearchArticle-JPCB/figure-materials/Example-Slow\ Dwelling-bandpass-RNA.tif"
+video = imread(path)
