799368776466e99994d45f3459c7aeafe6e9c22d
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-02-07 12:08:29 -0500

    Update video2trackoverlay.py

--- video2trackoverlay.py
+++ video2trackoverlay.py
@@ -8,7 +8,13 @@ import matplotlib.pyplot as plt
 path = "/Users/GGM/Documents/Graduate_Work/Nils_Walter_Lab/Writing/MyPublications/ResearchArticle-JPCB/figure-materials/Example-Slow Dwelling-bandpass-RNA.tif"
 frames = pims.open(path)
 
+# testing code
+# index = 6
+# f = tp.locate(frames[index], diameter=5, separation=3, minmass=5e5, preprocess=False)
+# tp.annotate(f, frames[index])
 
-spots = tp.batch(frames, diameter=5, separation=3, minmass=1)
+
+spots = tp.batch(frames, diameter=5, separation=3, minmass=5e5, preprocess=False)
 tracks = tp.link(spots, search_range=3)
 tracks_filtered = tp.filter_stubs(tracks, threshold=5)
+tp.plot_traj(tracks_filtered)
