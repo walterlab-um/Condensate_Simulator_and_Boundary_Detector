5a67c1b5f36c8cba2dc51bf2fa924c07d7152fbb
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-02-06 23:56:55 -0500

    Update video2trackoverlay.py

--- video2trackoverlay.py
+++ video2trackoverlay.py
@@ -1,4 +1,14 @@
-from tifffile import imread, imwrite
+import numpy as np
+import pandas as pd
+import pims
+import trackpy as tp
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-path = "/Users/GGM/Documents/Graduate_Work/Nils_Walter_Lab/Writing/MyPublications/ResearchArticle-JPCB/figure-materials/Example-Slow\ Dwelling-bandpass-RNA.tif"
-video = imread(path)
+path = "/Users/GGM/Documents/Graduate_Work/Nils_Walter_Lab/Writing/MyPublications/ResearchArticle-JPCB/figure-materials/Example-Slow Dwelling-bandpass-RNA.tif"
+frames = pims.open(path)
+
+
+spots = tp.batch(frames, diameter=5, separation=3, minmass=1)
+tracks = tp.link(spots, search_range=3)
+tracks_filtered = tp.filter_stubs(tracks, threshold=5)
