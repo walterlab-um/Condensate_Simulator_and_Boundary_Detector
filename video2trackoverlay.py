70b6ec019f38846ed2aeb6d48e8ffe399a54cc18
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-02-09 17:23:29 -0500

    v1.2 color; circle on imshow
    use circle patch rather than scatter plot to show the recognized boundary for condensates; color the tracks the same way as imshow

--- video2trackoverlay.py
+++ video2trackoverlay.py
@@ -2,10 +2,10 @@ import os
 from os.path import join, dirname
 import shutil
 import numpy as np
-import pandas as pd
 import pims
 import trackpy as tp
 from matplotlib.cm import get_cmap
+from matplotlib.patches import Circle
 import matplotlib.pyplot as plt
 from rich.progress import track
 
@@ -55,88 +55,86 @@ for idx in track(range(frames_condensate.shape[0])):
     if idx % 5 != 0:
         continue
 
-    # plot condensate channel
-    img = frames_condensate[idx]
-    fname_save = "frame_" + str(idx) + "_condensate.png"
+    img_condensate = frames_condensate[idx]
+    img_RNA = frames_RNA[idx]
+    current_track_condensate = tracks_condensate[tracks_condensate["frame"] <= idx]
+    x_condensate = current_track_condensate.x.to_numpy(dtype=float)
+    y_condensate = current_track_condensate.y.to_numpy(dtype=float)
+    size_condensate = current_track_condensate["size"].to_numpy(dtype=float)[-1]
+    current_track_RNA = tracks_RNA[tracks_RNA["frame"] <= idx]
+    x_RNA = current_track_RNA.x.to_numpy(dtype=float)
+    y_RNA = current_track_RNA.y.to_numpy(dtype=float)
+    size_RNA = current_track_RNA["size"].to_numpy(dtype=float)[-1]
 
-    plt.figure(figsize=(5, 5), dpi=300)
-    vmin, vmax = np.percentile(img, (0.05, 99))
-    plt.imshow(img, cmap="Blues", vmin=vmin, vmax=vmax)
+    color_con = get_cmap("Blues")(0.9)
+    color_RNA = get_cmap("Reds")(0.9)
+
+    # plot condensate channel
+    fname_save = "frame_" + str(idx) + "_condensate.png"
+    fig, ax = plt.subplots(figsize=(5, 5), dpi=300)
+    vmin, vmax = np.percentile(img_condensate, (0.05, 99))
+    ax.imshow(img_condensate, cmap="Blues", vmin=vmin, vmax=vmax)
+    spot = Circle(
+        (x_condensate[-1], y_condensate[-1]),
+        size_condensate,
+        color=color_con,
+        ls="--",
+        fill=False,
+        lw=2,
+    )
+    ax.add_patch(spot)
     plt.axis("scaled")
     # plt.axis("off")
     plt.gca().invert_yaxis()  # must invert AFTER xlim, otherwise it goes back uninverted
     plt.savefig(fname_save, format="png", bbox_inches="tight")
     plt.close()
-
-    # plot condensate track
-    # current_track = tracks_condensate[tracks_condensate["frame"] <= idx]
-    # cmap = get_cmap("Blues")
-    # color = cmap(0.9)
-    # fname_save = "frame_" + str(idx) + "_condensate_track.png"
-
-    # plt.figure(figsize=(5, 5), dpi=300)
-    # x = current_track.x.to_numpy(dtype=float)
-    # y = current_track.y.to_numpy(dtype=float)
-    # size = current_track["size"].to_numpy(dtype=float)[-1]
-    # plt.plot(x, y, ls="-", lw=1, color=color, alpha=0.3)
-    # plt.scatter(x[-1], y[-1], s=size * 10000, color=color, alpha=0.3)
-    # plt.axis("scaled")
-    # plt.xlim(0, frames_condensate.shape[1])
-    # plt.ylim(0, frames_condensate.shape[2])
-    # plt.savefig(fname_save, format="png", bbox_inches="tight")
-    # plt.close()
 
     # plot RNA channel
-    img = frames_RNA[idx]
     fname_save = "frame_" + str(idx) + "_RNA.png"
-
-    plt.figure(figsize=(5, 5), dpi=300)
-    vmin, vmax = np.percentile(img, (0.05, 99))
-    plt.imshow(img, cmap="Reds", vmin=vmin, vmax=vmax)
+    fig, ax = plt.subplots(figsize=(5, 5), dpi=300)
+    vmin, vmax = np.percentile(img_RNA, (0.05, 99))
+    plt.imshow(img_RNA, cmap="Reds", vmin=vmin, vmax=vmax)
+    spot = Circle(
+        (x_RNA[-1], y_RNA[-1]),
+        size_RNA,
+        color=color_RNA,
+        ls="--",
+        fill=False,
+        lw=2,
+    )
+    ax.add_patch(spot)
     plt.axis("scaled")
     # plt.axis("off")
     plt.gca().invert_yaxis()  # must invert AFTER xlim, otherwise it goes back uninverted
     plt.savefig(fname_save, format="png", bbox_inches="tight")
     plt.close()
 
-    # plot RNA track
-    # current_track = tracks_RNA[tracks_RNA["frame"] <= idx]
-    # color = "firebrick"
-    # fname_save = "frame_" + str(idx) + "_RNA_track.png"
-
-    # plt.figure(figsize=(5, 5), dpi=300)
-    # x = current_track.x.to_numpy(dtype=float)
-    # y = current_track.y.to_numpy(dtype=float)
-    # plt.plot(x, y, ls="-", lw=1, color=color)
-    # plt.scatter(x[-1], y[-1], s=200, color=color)
-    # plt.axis("scaled")
-    # plt.xlim(0, frames_condensate.shape[1])
-    # plt.ylim(0, frames_condensate.shape[2])
-    # plt.savefig(fname_save, format="png", bbox_inches="tight")
-    # plt.close()
-
     # plot overlay tracks
-    current_track = tracks_condensate[tracks_condensate["frame"] <= idx]
-    cmap = get_cmap("Blues")
-    color = cmap(0.9)
     fname_save = "frame_" + str(idx) + "_track.png"
+    fig, ax = plt.subplots(figsize=(5, 5), dpi=300)
+    ax.plot(x_condensate, y_condensate, ls="-", lw=1, color=color_con)
+    spot = Circle(
+        (x_condensate[-1], y_condensate[-1]),
+        size_condensate,
+        color=color_con,
+        ls="--",
+        fill=False,
+        lw=2,
+    )
+    ax.add_patch(spot)
+    spot = Circle(
+        (x_condensate[-1], y_condensate[-1]),
+        size_condensate,
+        color=color_con,
+        alpha=0.3,
+        lw=0,
+    )
+    ax.add_patch(spot)
 
-    plt.figure(figsize=(5, 5), dpi=300)
-    x = current_track.x.to_numpy(dtype=float)
-    y = current_track.y.to_numpy(dtype=float)
-    size = current_track["size"].to_numpy(dtype=float)[-1]
-    plt.plot(x, y, ls="-", lw=1, color=color, alpha=0.3)
-    plt.scatter(x[-1], y[-1], s=size * 5000, color=color, alpha=0.3)
-
-    current_track = tracks_RNA[tracks_RNA["frame"] <= idx]
-    color = "firebrick"
-
-    x = current_track.x.to_numpy(dtype=float)
-    y = current_track.y.to_numpy(dtype=float)
-    plt.plot(x, y, ls="-", lw=1, color=color)
-    plt.scatter(x[-1], y[-1], s=200, color=color)
+    ax.plot(x_RNA, y_RNA, ls="-", lw=1, color=color_RNA)
+    plt.scatter(x_RNA[-1], y_RNA[-1], s=200, color=color_RNA)
     plt.axis("scaled")
-    plt.xlim(-0.5, frames_condensate.shape[1] - 0.5)
-    plt.ylim(-0.5, frames_condensate.shape[2] - 0.5)
+    plt.xlim(-0.5, frames_condensate.shape[2] - 0.5)
+    plt.ylim(-0.5, frames_condensate.shape[1] - 0.5)
     plt.savefig(fname_save, format="png", bbox_inches="tight")
     plt.close()
