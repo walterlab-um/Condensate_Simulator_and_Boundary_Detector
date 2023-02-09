9d5221d298a445117b8242b21791f689a17ec3bb
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-02-09 02:30:53 -0500

    v1.0
    individually plot condensates and RNA and the overlay tracks, as a time series

--- video2trackoverlay.py
+++ video2trackoverlay.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import pims
 import trackpy as tp
-import matplotlib.pylab as pl
+from matplotlib.cm import get_cmap
 import matplotlib.pyplot as plt
 from rich.progress import track
 
@@ -48,7 +48,7 @@ spots = tp.batch(
 )
 tracks = tp.link(spots, search_range=3)
 tracks_condensate = tp.filter_stubs(tracks, threshold=5)
-tracks_RNA.to_csv("tracks_RNA.csv", index=False)
+tracks_condensate.to_csv("tracks_condensate.csv", index=False)
 
 
 for idx in track(range(frames_condensate.shape[0])):
@@ -64,8 +64,27 @@ for idx in track(range(frames_condensate.shape[0])):
     plt.imshow(img, cmap="Blues", vmin=vmin, vmax=vmax)
     plt.axis("scaled")
     plt.axis("off")
+    plt.gca().invert_yaxis()  # must invert AFTER xlim, otherwise it goes back uninverted
     plt.savefig(fname_save, format="png", bbox_inches="tight")
     plt.close()
+
+    # plot condensate track
+    # current_track = tracks_condensate[tracks_condensate["frame"] <= idx]
+    # cmap = get_cmap("Blues")
+    # color = cmap(0.9)
+    # fname_save = "frame_" + str(idx) + "_condensate_track.png"
+
+    # plt.figure(figsize=(5, 5), dpi=300)
+    # x = current_track.x.to_numpy(dtype=float)
+    # y = current_track.y.to_numpy(dtype=float)
+    # size = current_track["size"].to_numpy(dtype=float)[-1]
+    # plt.plot(x, y, ls="-", lw=1, color=color, alpha=0.3)
+    # plt.scatter(x[-1], y[-1], s=size * 10000, color=color, alpha=0.3)
+    # plt.axis("scaled")
+    # plt.xlim(0, frames_condensate.shape[1])
+    # plt.ylim(0, frames_condensate.shape[2])
+    # plt.savefig(fname_save, format="png", bbox_inches="tight")
+    # plt.close()
 
     # plot RNA channel
     img = frames_RNA[idx]
@@ -76,9 +95,48 @@ for idx in track(range(frames_condensate.shape[0])):
     plt.imshow(img, cmap="Reds", vmin=vmin, vmax=vmax)
     plt.axis("scaled")
     plt.axis("off")
+    plt.gca().invert_yaxis()  # must invert AFTER xlim, otherwise it goes back uninverted
     plt.savefig(fname_save, format="png", bbox_inches="tight")
     plt.close()
 
+    # plot RNA track
+    # current_track = tracks_RNA[tracks_RNA["frame"] <= idx]
+    # color = "firebrick"
+    # fname_save = "frame_" + str(idx) + "_RNA_track.png"
 
-# prepare colors for both tracks
-colors_condensate = pl.cm.jet(np.linspace(0, 1, n))
+    # plt.figure(figsize=(5, 5), dpi=300)
+    # x = current_track.x.to_numpy(dtype=float)
+    # y = current_track.y.to_numpy(dtype=float)
+    # plt.plot(x, y, ls="-", lw=1, color=color)
+    # plt.scatter(x[-1], y[-1], s=200, color=color)
+    # plt.axis("scaled")
+    # plt.xlim(0, frames_condensate.shape[1])
+    # plt.ylim(0, frames_condensate.shape[2])
+    # plt.savefig(fname_save, format="png", bbox_inches="tight")
+    # plt.close()
+
+    # plot overlay tracks
+    current_track = tracks_condensate[tracks_condensate["frame"] <= idx]
+    cmap = get_cmap("Blues")
+    color = cmap(0.9)
+    fname_save = "frame_" + str(idx) + "_track.png"
+
+    plt.figure(figsize=(5, 5), dpi=300)
+    x = current_track.x.to_numpy(dtype=float)
+    y = current_track.y.to_numpy(dtype=float)
+    size = current_track["size"].to_numpy(dtype=float)[-1]
+    plt.plot(x, y, ls="-", lw=1, color=color, alpha=0.3)
+    plt.scatter(x[-1], y[-1], s=size * 10000, color=color, alpha=0.3)
+
+    current_track = tracks_RNA[tracks_RNA["frame"] <= idx]
+    color = "firebrick"
+
+    x = current_track.x.to_numpy(dtype=float)
+    y = current_track.y.to_numpy(dtype=float)
+    plt.plot(x, y, ls="-", lw=1, color=color)
+    plt.scatter(x[-1], y[-1], s=200, color=color)
+    plt.axis("scaled")
+    plt.xlim(0, frames_condensate.shape[1])
+    plt.ylim(0, frames_condensate.shape[2])
+    plt.savefig(fname_save, format="png", bbox_inches="tight")
+    plt.close()
