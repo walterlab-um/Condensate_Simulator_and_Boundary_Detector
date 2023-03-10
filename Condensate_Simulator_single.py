55b78026fa52916fe44acd740a59cf0d3658afe8
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-03-10 00:38:10 -0500

    Major update
    create simulation box thickness by r and sigma (saving running time), 3d PSF convolution, slice of simulation box by depth of focus, remove shot noise, remove laser power

--- Condensate_Simulator_single.py
+++ Condensate_Simulator_single.py
@@ -8,15 +8,15 @@ from skimage.util import random_noise, img_as_uint, img_as_float
 from rich.progress import track
 
 ##################################
-folder_save = dirname(realpath(__file__))
+# folder_save = dirname(realpath(__file__))
+folder_save = "/Volumes/AnalysisGG/PROCESSED_DATA/JPCB-CondensateBoundaryDetection"
 ## FOV parameters
-fovsize = 5000  # unit: nm
-truth_img_pxlsize = 10  # unit: nm
-real_img_pxlsize = 100  # unit: nm, must be an integer multiple of truth_img_pxlsize
+fovsize = 1000  # unit: nm
+truth_box_pxlsize = 10  # unit: nm
+real_img_pxlsize = 100  # unit: nm, must be an integer multiple of truth_box_pxlsize
 N_fov = 1  # number of total im
 
 ## Imaging system parameters
-laser_power = 11  # mimic experiment data intensity by changing this
 # Microscope parameters
 depth_of_focus = 500  # unit, nm
 Numerical_Aperature = 1.5
@@ -28,17 +28,23 @@ gaussian_noise_mean = 400
 gaussian_noise_sigma = 5  # white noise
 # PSF approximations, Ref doi: 10.1364/AO.46.001819
 k_em = (2 * np.pi) / emission_wavelength
-sigma_lateral = np.sqrt(2) / (k_em * Numerical_Aperature)
-sigma_axial = (2 * np.sqrt(6) * refractive_index) / (k_em * Numerical_Aperature**2)
+sigma_lateral = np.sqrt(2) / (k_em * Numerical_Aperature)  # x and y
+sigma_axial = (2 * np.sqrt(6) * refractive_index) / (
+    k_em * Numerical_Aperature**2
+)  # z
+# convert values to pixels
+sigma_lateral = sigma_lateral / truth_box_pxlsize
+sigma_axial = sigma_axial / truth_box_pxlsize
+depth_of_focus = depth_of_focus / truth_box_pxlsize
 
 
 ## Condensate parameters
 # condensate size follows Gaussian distribution
-condensate_r_ave = 200  # average size of condensates, unit: nm
+condensate_r_ave = 300  # average size of condensates, unit: nm
 condensate_r_sigma = condensate_r_ave / 5
 pad_size = 200  # push condensates back from FOV edges. unit: nm
-C_condensed = 100  # N.A. unit
-C_dilute = 30
+C_condensed = 500  # N.A. unit
+C_dilute = 10
 
 
 #################################################
@@ -79,15 +85,21 @@ df_groundtruth.to_csv(path_save, index=False)
 for current_fov in track(index):
     # extract condensate ground truth for current FOV
     df_current = df_groundtruth[df_groundtruth.FOVindex == current_fov]
-    fovsize_pxl = int(fovsize / truth_img_pxlsize)
+    fovsize_truth = int(fovsize / truth_box_pxlsize)
+    # minimal Z range for convolution should be r + 3*sigma_axial
+    r_truth = df_current["r_nm"].squeeze() / truth_box_pxlsize
+    z_min = int(fovsize_truth / 2 - r_truth - 3 * sigma_axial)
+    z_max = int(fovsize_truth / 2 + r_truth + 3 * sigma_axial)
     pxl_x, pxl_y, pxl_z = np.meshgrid(
-        np.arange(fovsize_pxl), np.arange(fovsize_pxl), np.arange(fovsize_pxl)
+        np.arange(fovsize_truth),
+        np.arange(fovsize_truth),
+        np.arange(z_min, z_max),
     )
-    center_x_pxl = df_current.x_nm.squeeze() / truth_img_pxlsize
-    center_y_pxl = df_current.y_nm.squeeze() / truth_img_pxlsize
-    center_z_pxl = (fovsize / 2) / truth_img_pxlsize
-    r_pxl = df_current.r_nm.squeeze() / truth_img_pxlsize
-    depth_of_focus_pxl = depth_of_focus / truth_img_pxlsize
+    center_x_pxl = df_current.x_nm.squeeze() / truth_box_pxlsize
+    center_y_pxl = df_current.y_nm.squeeze() / truth_box_pxlsize
+    center_z_pxl = (fovsize / 2) / truth_box_pxlsize
+    r_pxl = df_current.r_nm.squeeze() / truth_box_pxlsize
+    depth_of_focus_pxl = depth_of_focus / truth_box_pxlsize
 
     #################################################
     # Step 2: Ground truth high-resolution volume "image"
@@ -98,46 +110,58 @@ for current_fov in track(index):
         + (pxl_z - center_z_pxl) ** 2
     )
     condensate_mask = distance_square < r_pxl**2
-    img_truth = condensate_mask * C_condensed + (1 - condensate_mask) * C_dilute
-    img_truth = img_truth.astype("uint16")
+    truth_box = condensate_mask * C_condensed + (1 - condensate_mask) * C_dilute
+    truth_box = truth_box.astype("uint16")
 
-    # Save ground truth, high-resolution image
+    # Save ground truth box
     path_save = join(folder_save, "Truth-FOVindex-" + str(current_fov) + ".tif")
-    imwrite(path_save, img_truth, imagej=True)
+    imwrite(path_save, truth_box, imagej=True)
 
     #################################################
     # Step 3: simulated 'real' image
 
     # Convolution with Gaussian approximation of PSF. Interference happens irrespective of optical magnification or pixel size and thus should be performed first.
-    sigma_PSF_pxl = sigma_PSF / real_img_pxlsize
-    img_PSFconvolved = gaussian_filter(img_truth, sigma=sigma_PSF_pxl)
+    box_PSFconvolved = gaussian_filter(
+        truth_box, sigma=[sigma_lateral, sigma_lateral, sigma_axial]
+    )
+    # use depth of focus to generate an image
+    z_middle = (z_max - z_min) / 2
+    img_PSFconvolved = np.sum(
+        box_PSFconvolved[
+            :,
+            :,
+            int(z_middle - depth_of_focus / 2) : int(z_middle + depth_of_focus / 2) + 1,
+        ],
+        axis=2,
+    ).astype("uint16")
+
+    # Save convolution products
+    path_save = join(
+        folder_save, "TruthBoxConvolved-FOVindex-" + str(current_fov) + ".tif"
+    )
+    imwrite(path_save, box_PSFconvolved, imagej=True)
+    path_save = join(folder_save, "TruthImg-FOVindex-" + str(current_fov) + ".tif")
+    imwrite(path_save, img_PSFconvolved, imagej=True)
+
     # Magnification adjustment. Re-adjust the high-res image back to practically low-res image by integration
-    fovsize_pxl = int(fovsize / real_img_pxlsize)
-    pxl_x, pxl_y = np.meshgrid(np.arange(fovsize_pxl), np.arange(fovsize_pxl))
-    ratio = int(real_img_pxlsize / truth_img_pxlsize)
+    fovsize_real = int(fovsize / real_img_pxlsize)
+    ratio = int(real_img_pxlsize / truth_box_pxlsize)
     lst_pxl_value = []
-    for xx in np.arange(fovsize_pxl):
-        for yy in np.arange(fovsize_pxl):
+    for xx in np.arange(fovsize_real):
+        for yy in np.arange(fovsize_real):
             lst_pxl_value.append(
-                np.sum(
+                np.mean(
                     img_PSFconvolved[
                         ratio * xx : ratio * (xx + 1), ratio * yy : ratio * (yy + 1)
                     ]
                 )
                 / (ratio**2)
             )
-    # img_shrinked = (
-    #     np.array(lst_pxl_value).reshape((fovsize_pxl, fovsize_pxl)) * laser_power
-    # )
-    img_shrinked = np.array(lst_pxl_value).reshape((fovsize_pxl, fovsize_pxl))
-    poisson_noise = poisson(lam=poisson_noise_lambda, size=img_shrinked.shape)
-    poisson_noise[poisson_noise > 65535] = 0  # Trim off extreme values exceeding uint16
-    img_shot = img_shrinked.astype("uint16") + poisson_noise.astype("uint16")
-    # img_shot = img_as_uint(random_noise(img_shrinked.astype("uint16"), mode="poisson"))
+    img_shrinked = np.array(lst_pxl_value).reshape((fovsize_real, fovsize_real))
     gaussian_noise = normal(
         gaussian_noise_mean, gaussian_noise_sigma, img_shrinked.shape
     )
-    img_shot_gaussian = img_shot + gaussian_noise.astype("uint16")
+    img_gaussian = img_shrinked + gaussian_noise.astype("uint16")
 
     path_save = join(folder_save, "shrinked-FOVindex-" + str(current_fov) + ".tif")
     imwrite(
@@ -146,16 +170,16 @@ for current_fov in track(index):
         imagej=True,
     )
 
-    path_save = join(folder_save, "shot-FOVindex-" + str(current_fov) + ".tif")
+    path_save = join(folder_save, "gaussian-FOVindex-" + str(current_fov) + ".tif")
     imwrite(
         path_save,
-        img_shot,
+        img_gaussian.astype("uint16"),
         imagej=True,
     )
 
-    path_save = join(folder_save, "final-FOVindex-" + str(current_fov) + ".tif")
-    imwrite(
-        path_save,
-        img_shot_gaussian,
-        imagej=True,
-    )
+    # path_save = join(folder_save, "final-FOVindex-" + str(current_fov) + ".tif")
+    # imwrite(
+    #     path_save,
+    #     img_shot_gaussian,
+    #     imagej=True,
+    # )
