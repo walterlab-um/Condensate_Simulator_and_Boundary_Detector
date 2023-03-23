25bc735afd5676f3715ddef68ac9b4c11bdd940d
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-03-22 23:52:40 -0400

    v1.0, working!
    r, PC, success, deviation center, rmsd edge, fold deviation in area and PC

--- Validation-Edge.py
+++ Validation-Edge.py
@@ -1,4 +1,4 @@
-from tkinter import filedialog as fd
+from tifffile import imread
 import os
 import pickle
 import cv2
@@ -8,109 +8,124 @@ import seaborn as sns
 
 sns.set(color_codes=True, style="white")
 
-folder = fd.askdirectory(
-    initialdir="/Volumes/AnalysisGG/PROCESSED_DATA/JPCB-CondensateBoundaryDetection/"
-)
-# folder = "/Volumes/AnalysisGG/PROCESSED_DATA/JPCB-CondensateBoundaryDetection/mimic_Dcp1a_HOPS/test_100"
-os.chdir(folder)
-
-
-path_pkl = [f for f in os.listdir(folder) if f.endswith(".pkl")][0]
-path_groundtruth = [f for f in os.listdir(folder) if f.endswith("groundtruth.csv")][0]
-
+#################################################
+# Inputs
 real_img_pxlsize = 100  # unit: nm, must be an integer multiple of truth_img_pxlsize
-fovsize = 5000  # unit: nm
+fovsize = 2000  # unit: nm
+folder = (
+    "/Volumes/AnalysisGG/PROCESSED_DATA/JPCB-CondensateBoundaryDetection/Simulated-32"
+)
+os.chdir(folder)
+path_groundtruth = "groundtruth.csv"
+path_pkl = "Method-1-Denoise_Threshold/Contours_Denoise_Threshold.pkl"
+# path_pkl = "Method-2-Canny/Contours_Canny.pkl"
 
-lst_index, lst_contours = pickle.load(open(path_pkl, "rb"))
-df_truth = pd.read_csv(path_groundtruth)
+
+#################################################
+# Functions
+def cnt_fill(imgshape, cnt):
+    # create empty image
+    mask = np.zeros(imgshape, dtype=np.uint8)
+    # draw contour
+    cv2.fillPoly(mask, [cnt], (255))
+
+    return mask
 
 
 def when_failed():
-    global success, areas, aspect_ratios, distance2center, distance2edge, centroid, real_center
+    global success, rmsd_center, rmsd_edge, area_fold_deviation, fold_deviation_pc
     success.append(False)
-    areas.append(np.nan)
-    aspect_ratios.append(np.nan)
-    distance2center.append(np.nan)
-    distance2edge.append(np.nan)
-    centroid.append(np.nan)
-    real_center.append(np.nan)
+    deviation_center.append(np.nan)
+    rmsd_edge.append(np.nan)
+    area_fold_deviation.append(np.nan)
+    fold_deviation_pc.append(np.nan)
 
 
+#################################################
+# Main
+lst_index, lst_contours = pickle.load(open(path_pkl, "rb"))
+df_truth = pd.read_csv(path_groundtruth)
+
+lst_truth_r = []
+lst_truth_pc = []  # partition coefficient
 success = []
-areas = []  # area of contours
-aspect_ratios = []
-distance2center = []
-distance2edge = []
-centroid = []
-real_center = []
+deviation_center = []
+rmsd_edge = []
+area_fold_deviation = []  # with sign
+# difference in partition coefficient, assume outside C_dilute=1, so directly the average intensity inside, with sign
+fold_deviation_pc = []
 for index, contours in zip(np.array(lst_index, dtype=int), lst_contours):
-    if len(contours) > 1:
-        when_failed()
-        continue
-
     # retreive ground truth
     row = df_truth[df_truth.FOVindex == index]
-    truth_x_nm = row.x_nm.squeeze()
-    truth_y_nm = row.y_nm.squeeze()
-    truth_r_nm = row.r_nm.squeeze()
+    truth_x_nm = row["x_nm"].squeeze()
+    truth_y_nm = row["y_nm"].squeeze()
+    truth_r_nm = row["r_nm"].squeeze()
+    truth_pc = row["C_condensed"].squeeze()
+    detected_contour = contours[0]
+    lst_truth_r.append(truth_r_nm)
+    lst_truth_pc.append(truth_pc)
 
-    # area
-    cnt = contours[0]
-    area = cv2.contourArea(cnt) * real_img_pxlsize**2
-    # cnt could be the whole FOV when it failed to detect spot
-    if area > 0.8 * fovsize**2:
+    # fail if more than 1 condensate or no condensate was detected
+    if len(contours) > 1 | len(contours) == 0:
+        when_failed()
+        continue
+    # fail if the contour is just the whole field of view
+    if cv2.contourArea(detected_contour) * real_img_pxlsize**2 > 0.8 * fovsize**2:
         when_failed()
         continue
 
-    # aspect ratio
-    rect = cv2.minAreaRect(cnt)
-    (x, y), (width, height), angle = rect
-    if max(width, height) == 0:
+    # calculate the deviation of condensate center
+    M = cv2.moments(detected_contour)
+    if M["m00"] == 0:
         when_failed()
         continue
-
-    # distance to center
-    M = cv2.moments(cnt)
     cx = int(M["m10"] / M["m00"]) * real_img_pxlsize
     cy = int(M["m01"] / M["m00"]) * real_img_pxlsize
     d2center = np.sqrt((cx - truth_x_nm) ** 2 + (cy - truth_y_nm) ** 2)
-    distance2center.append(d2center)
-
-    # save
-    success.append(True)
-    areas.append(area)
-    aspect_ratios.append(min(width, height) / max(width, height))
-    centroid.append((cx, cy))
-    real_center.append((truth_x_nm, truth_y_nm))
-
-    # distance to edge, for each vertex
-    cnt_reshaped = np.reshape(cnt, (cnt.shape[0], cnt.shape[2]))
-    d2edge = []
-
+    # calculate the RMSD of detected edge to real condensate edge
+    cnt_reshaped = np.reshape(
+        detected_contour, (detected_contour.shape[0], detected_contour.shape[2])
+    )
+    d2edge_squared = []
     for cnt_x, cnt_y in cnt_reshaped:
-        d2edge.append(
+        d2edge_squared.append(
             np.sqrt(
                 (cnt_x * real_img_pxlsize - truth_x_nm) ** 2
                 + (cnt_y * real_img_pxlsize - truth_y_nm) ** 2
             )
             - truth_r_nm
         )
-    distance2edge.append(np.array(d2edge))
+    rmsd = np.sqrt(np.mean(d2edge_squared))
+    # calculate the relative deviation in area
+    area = cv2.contourArea(detected_contour) * real_img_pxlsize**2
+    # calculate partition coefficient
+    img = imread("Simulated-FOVindex-" + str(index) + ".tif")
+    mask_in = cnt_fill(img.shape, detected_contour)
+    mask_out = 1 - mask_in
+    partition_coefficient = (
+        cv2.mean(img, mask=mask_in)[0] / cv2.mean(img, mask=mask_out)[0]
+    )
+    # save
+    success.append(True)
+    deviation_center.append(d2center)
+    rmsd_edge.append(rmsd)
+    area_fold_deviation.append(area / (np.pi * truth_r_nm**2))
+    fold_deviation_pc.append(partition_coefficient / truth_pc)
 
 
 df_save = pd.DataFrame(
     {
         "index": lst_index,
+        "truth_r": lst_truth_r,
+        "truth_pc": lst_truth_pc,
         "success": success,
-        "area_nm2": areas,
-        "aspect_ratio": aspect_ratios,
-        "distance2center": distance2center,
-        "distance2edge": distance2edge,
-        "centroid": centroid,
-        "real_center": real_center,
+        "deviation_center": deviation_center,
+        "rmsd_edge": rmsd_edge,
+        "fold_deviation_area": area_fold_deviation,
+        "fold_deviation_PC": fold_deviation_pc,
     },
     dtype=object,
 )
 
-path_save = path_pkl[:-4] + "_validation_results.csv"
+path_save = path_pkl[:-4] + "_results.csv"
 df_save.to_csv(path_save, index=False)
