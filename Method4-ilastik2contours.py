19bd47b442abc0fbb5fb033b7f977f640909f24d
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-03-22 22:53:36 -0400

    v1.0 fully functional
    skip images with no condensate detected, and extract the correct boundary

--- Method4-ilastik2contours.py
+++ Method4-ilastik2contours.py
@@ -1,11 +1,8 @@
-from tkinter import filedialog as fd
 import os
-from os.path import join, exists
+from os.path import join
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage.filters import threshold_otsu
-from scipy.ndimage import gaussian_filter
 from tifffile import imread
 import pickle
 from rich.progress import track
@@ -25,29 +22,20 @@ switch_plot = True  # a switch to turn off plotting
 
 ####################################
 # Functions
-def cnt_fill(imgshape, cnt):
-    # create empty image
-    mask = np.zeros(imgshape, dtype=np.uint8)
-    # draw contour
-    cv2.fillPoly(mask, [cnt], (255))
-
-    return mask
-
-
 def pltcontours(img, contours, fsave):
     global rescale_contrast, plow, phigh
     plt.figure(dpi=300)
     # Contrast stretching
     vmin, vmax = np.percentile(img, (plow, phigh))
     plt.imshow(img, cmap="Blues", vmin=vmin, vmax=vmax)
-    for cnt in contours:
-        x = cnt[:, 0][:, 0]
-        y = cnt[:, 0][:, 1]
-        plt.plot(x, y, "-", color="black", linewidth=2)
-        # still the last closing line will be missing, get it below
-        xlast = [x[-1], x[0]]
-        ylast = [y[-1], y[0]]
-        plt.plot(xlast, ylast, "-", color="black", linewidth=2)
+    cnt = contours[0]
+    x = cnt[:, 0][:, 0]
+    y = cnt[:, 0][:, 1]
+    plt.plot(x, y, "-", color="black", linewidth=2)
+    # still the last closing line will be missing, get it below
+    xlast = [x[-1], x[0]]
+    ylast = [y[-1], y[0]]
+    plt.plot(xlast, ylast, "-", color="black", linewidth=2)
     plt.xlim(0, img.shape[0])
     plt.ylim(0, img.shape[1])
     plt.tight_layout()
@@ -57,36 +45,32 @@ def pltcontours(img, contours, fsave):
     plt.close()
 
 
-def cnt2mask(imgshape, contours):
-    # create empty image
-    mask = np.zeros(imgshape, dtype=np.uint8)
-    # draw contour
-    for cnt in contours:
-        cv2.fillPoly(mask, [cnt], (255))
-    return mask
-
-
 ####################################
 # Main
 for subfolder in lst_subfolders:
+    print("Now working on:", subfolder)
     lst_tifs = [join(subfolder, f) for f in os.listdir(subfolder) if f.endswith(".tif")]
     lst_index = []
     lst_contours = []
     for fpath in track(lst_tifs):
-        index = fpath.split("FOVindex-")[-1].split("Simple")[0]
+        index = fpath.split("FOVindex-")[-1].split("_Simple")[0]
         img_raw = imread("Simulated-FOVindex-" + index + ".tif")
         mask = imread(fpath)
-        mask = mask > 1
+        mask = 2 - mask  # background label=2, condensate label=1
+
+        if mask.sum() == 0:  # so no condensate
+            continue
+
         # find contours coordinates in binary edge image. contours here is a list of np.arrays containing all coordinates of each individual edge/contour.
         contours_final, _ = cv2.findContours(
-            mask * 1, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE
+            mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE
         )
 
         lst_index.append(index)
         lst_contours.append(contours_final)
 
         if switch_plot:
-            pltcontours(img_raw, contours_final, fpath[:-4] + "_Denoise_Threshold.png")
+            pltcontours(img_raw, contours_final, fpath[:-4] + ".png")
         else:
             continue
 
