e8733a580002f76f280d7491269aab023f5960a2
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2023-03-23 15:57:30 -0400

    Create plot_sampling_coverage.py

---
+++ plot_sampling_coverage.py
@@ -0,0 +1,30 @@
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+sns.set(color_codes=True, style="white")
+
+###################################
+# Parameters
+bins = 10
+
+folder = (
+    "/Volumes/AnalysisGG/PROCESSED_DATA/JPCB-CondensateBoundaryDetection/Simulated-1024"
+)
+os.chdir(folder)
+data = pd.read_csv("groundtruth.csv", dtype=float)
+
+
+###################################
+# Main
+plt.figure(figsize=(5, 5), dpi=300)
+plt.scatter(data["r_nm"], data["C_condensed"], color="gray", alpha=0.5, s=1)
+plt.xlim(100, 600)
+plt.ylim(2, 10)
+plt.xlabel("Condensate Radius, nm", weight="bold")
+plt.ylabel("Partition Coefficient", weight="bold")
+plt.title("Sampling Coverage", weight="bold")
+plt.tight_layout()
+plt.savefig("Sampling Coverage.png", format="png", bbox_inches="tight")
+plt.close()
